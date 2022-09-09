8bb0e3a0b6dc39c70fbd21d1ed6565615ab50b85
Jiny-world <hellozin31@naver.com>
2022-09-09 17:29:49 +0900

    라우터 베이스경로 수정

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,29 @@
 const BASE_URL = "https://api.coinpaprika.com/v1";
 
 export async function fetchCoins() {
+  console.log("fetchCoins");
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
 
 export async function fetchCoinInfo(coinId: string) {
+  console.log("fetchCoinInfo");
+
   return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
     response.json()
   );
 }
 
 export async function fetchCoinTickers(coinId: string) {
+  console.log("fetchCoinTickers");
+
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
     response.json()
   );
 }
 
 export async function fetchCoinHistory(coinId: string) {
+  console.log("fetchCoinHistory");
+
   return fetch(
     `https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`
   ).then((response) => response.json());
